119673ac62f287a8162b31bc2caa788c11e86f69
Marius Cocoi <marius.cocoi@evozon.com>
2015-07-02 14:12:31 +0300

    addapted startup.bat

--- startup.bat
+++ startup.bat
@@ -14,49 +14,6 @@ rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 rem See the License for the specific language governing permissions and
 rem limitations under the License.
 
-if "%OS%" == "Windows_NT" setlocal
-rem ---------------------------------------------------------------------------
-rem Start script for the CATALINA Server
-rem
-rem $Id: startup.bat 895392 2010-01-03 14:02:31Z kkolinko $
-rem ---------------------------------------------------------------------------
 
-rem Guess CATALINA_HOME if not defined
-set "CURRENT_DIR=%cd%"
-rem if not "%CATALINA_HOME%" == "" goto gotHome
-set "CATALINA_HOME=%CURRENT_DIR%"
-if exist "%CATALINA_HOME%\bin\catalina.bat" goto okHome
-cd ..
-set "CATALINA_HOME=%cd%"
-cd "%CURRENT_DIR%"
-:gotHome
-if exist "%CATALINA_HOME%\bin\catalina.bat" goto okHome
-echo The CATALINA_HOME environment variable is not defined correctly
-echo This environment variable is needed to run this program
-goto end
-:okHome
 
-set "EXECUTABLE=%CATALINA_HOME%\bin\catalina.bat"
-
-rem Check that target executable exists
-if exist "%EXECUTABLE%" goto okExec
-echo Cannot find "%EXECUTABLE%"
-echo This file is needed to run this program
-goto end
-:okExec
-
-rem Get remaining unshifted command line arguments and save them in the
-set CMD_LINE_ARGS=
-:setArgs
-if ""%1""=="""" goto doneSetArgs
-set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
-shift
-goto setArgs
-:doneSetArgs
-
-set JPDA_ADDRESS=8001
-echo executable = %EXECUTABLE%
-echo CMD_LINE_ARGS = %CMD_LINE_ARGS%
-call "%EXECUTABLE%" jpda start %CMD_LINE_ARGS%
-
-:end
+echo This is a test for portal\startup.bat
